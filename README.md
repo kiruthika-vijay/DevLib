0b2d06e55bcf882d1f3d7cdc0f339d31bc612e7a
kiruthika-vijay <kiruthikasrivijay03@gmail.com>
2023-06-02 13:07:44 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+Build a simple library management page where
+○ Users should be able to see the list of books.
+■ Instead of loading all the results on the page, perform paging on the
+list of books – either implement 10 results per page or load on scroll
+will be a bonus.
+
+○ Users should be able to filter the list of books based on Title, Author, Subject
+and Publish - date
+■ User should be able to see the count of books in based on each
+criteria [ Title, Author, Subject and publish-date]
---
+++ member.js
@@ -0,0 +1,18 @@
+var members = [
+    { name: 'John Doe', age: 30, address: '123 Main St' },
+    { name: 'Jane Smith', age: 25, address: '456 Elm St' },
+    { name: 'Mike Johnson', age: 40, address: '789 Oak St' },
+    { name: 'Sarah Thompson', age: 28, address: '567 Pine St' },
+    { name: 'David Brown', age: 35, address: '321 Maple Ave' },
+    { name: 'Emily Wilson', age: 32, address: '876 Cedar Rd' },
+    { name: 'Alex Turner', age: 27, address: '654 Birch Ln' },
+    { name: 'Olivia Davis', age: 23, address: '987 Elmwood Dr' },
+    { name: 'Daniel Harris', age: 41, address: '234 Oakwood Ct' },
+    { name: 'Sophia Adams', age: 29, address: '543 Pinecrest Blvd' },
+    { name: 'Liam Walker', age: 24, address: '432 Maplewood Ave' },
+    { name: 'Ava Martin', age: 31, address: '789 Birchwood Dr' },
+    { name: 'Matthew Reed', age: 38, address: '123 Elmridge Rd' },
+    { name: 'Emma Green', age: 26, address: '345 Oakdale Ave' },
+    { name: 'Lucas Lee', age: 33, address: '678 Pinewood Dr' },
+    { name: 'Mia Wright', age: 22, address: '987 Cedarcrest Blvd' }
+  ];
